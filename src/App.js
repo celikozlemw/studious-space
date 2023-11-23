68c0a4dad4f7dbdb84e72e7402c091bf71317f18
ÖZLEM <celik.ozlem.05062003@gmail.com>
2023-11-23 03:03:20 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,18 @@
 import './App.css';
-
-
 function App() {
   function showMessage() {
     var message = document.getElementById("message").value;
     document.getElementById("output").innerHTML = message;
   }
   return (
-
-
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-         <span className="heart">😁</span> React
-        </p>
-        
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          
-<form action="#" id="myForm">
-      <textarea id="message" rows="1" cols="20" placeholder="deneme mesajınızı girin"></textarea>
-  <button type="button" onclick="showMessage()">Gönder</button>
-</form>
-<p id="output"></p>
 
-          </a>
-        </p>
-      </header>
+<img src="Octocat.png" className="App-logo" alt="logo" />
 
-   
+      <input id="message" type="text" placeholder="Mesajınızı yazın" />
+      <button onClick={showMessage}>Gönder</button>
+      <div id="output"></div>
     </div>
-
   );
 }
 export default App;
